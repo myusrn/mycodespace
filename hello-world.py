16f5e5b2babe4ac2cb66eff6d9e892a57a086114
Robert O'Brien <ob1@outlook.com>
2020-11-27 06:34:02 +0000

    adding source file

---
+++ hello-world.py
@@ -0,0 +1,3 @@
+import numpy # in terminal do one time execution of "pip install numpy"
+import pandas # in terminal do one time execution of "pip install pandas"
+print("hello cloud hosted browser based visual studio code python world")
